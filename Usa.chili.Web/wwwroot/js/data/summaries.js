957131e8ba55e259c7591a3ac830589e6ed99ac0
Justin Kirk <jkirk@traversconsultingus.com>
2019-09-10 12:57:00 -0500

    Fix tooltips

--- Usa.chili.Web/wwwroot/js/data/summaries.js
+++ Usa.chili.Web/wwwroot/js/data/summaries.js
@@ -1,6 +1,9 @@
 $(function () {
   $('#dayMonthSelection').select2(Core.select2Options);
-  $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="tooltip"]').tooltip({
+    boundary: 'span',
+    placement: 'left'
+  });
 });
 
 const App = new Vue({
