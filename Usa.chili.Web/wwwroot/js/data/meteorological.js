13240e5f6ba6d2529090fc7e793f4e9bb10e9410
johnbentley <jfb1721@jagmail.southalabama.edu>
2019-11-20 19:59:21 -0600

    created new vue for meteorological data

--- Usa.chili.Web/wwwroot/js/data/meteorological.js
+++ Usa.chili.Web/wwwroot/js/data/meteorological.js
@@ -8,6 +8,53 @@ const App = new Vue({
   el: '#app',
   data: function () {
     return {
+      meteorologicalData: [],
+      now: new Date()
+    }
+  },
+  created: function() {
+    const self = this;
+
+    // Get data on load
+    self.getData();
+
+    // Get data again every 5 minutes
+    setInterval(function() {
+      self.getData();
+    }, 300000);
+  },
+  computed: {
+    nowFormated: function() {
+      return moment(this.now).format('MM/DD/YYYY HH:mm:ss');
+    }
+  },
+  methods: {
+    getData: function() {
+      const self = this;
+
+      axios.get('/data/Station')
+      .then(function (response) {
+        self.realtimeData = response.data;
+        Vue.nextTick(function () {
+          self.now = new Date();
+
+          $('[data-toggle="tooltip"]').tooltip({
+            boundary: 'span',
+            placement: 'left'
+          });
+        });
+      })
+      .catch(function (error) {
+        console.log('getMeteorologicalData failed', error);
+      });
+    }
+  }
+});
+
+/*const App = new Vue({
+  el: '#app',
+  data: function () {
+    return {
       stations: [],
       model: {
         stationId: 1
@@ -17,4 +64,4 @@ const App = new Vue({
   created: function() {
     Core.populateStationDropdown(this, false);
   }
-});
+});*/
