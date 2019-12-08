d412dc8a6dfd6840722fe5974ef322b5f7435d84
Justin Kirk <thejuki@hotmail.com>
2019-12-08 10:43:31 -0600

    Add functionality to Meteorological Data page

--- Usa.chili.Web/wwwroot/js/data/meteorological.js
+++ Usa.chili.Web/wwwroot/js/data/meteorological.js
@@ -10,14 +10,60 @@ const App = new Vue({
     return {
       stations: [],
       model: {
-        stationId: Core.DEFAULT_STATION
-      }
+        stationId: Core.DEFAULT_STATION,
+        selectedVariables: [],
+      },
+      variables: [],
+      selectAllVariables: false
     }
   },
-  created: function() {
+  created: function () {
     Core.populateStationDropdown(this, false);
+
+    // Get Variable list from JSON file
+    this.getVariables();
+
   },
   methods: {
+    getVariables: function () {
+      const self = this;
+
+      axios.get('/json/variables.json')
+        .then(function (response) {
+          self.variables = response.data;
+        })
+        .catch(function (error) {
+          console.log('getVariables failed', error);
+        });
+    },
+    selectAll: function () {
+      const self = this;
+
+      self.model.selectedVariables = [];
+
+      if (self.selectAllVariables) {
+        self.variables.forEach(function (item) {
+          self.model.selectedVariables.push(item.name);
+        });
+      }
+    },
+    selectOne: function () {
+      if (this.model.selectedVariables
+          && this.model.selectedVariables.length > 0
+          && this.model.selectedVariables.length != this.variables.length) {
+        $('#selectAll').prop('indeterminate', true);
+      }
+      else if (this.model.selectedVariables
+        && this.model.selectedVariables.length > 0
+        && this.model.selectedVariables.length == this.variables.length) {
+        $('#selectAll').prop('indeterminate', false);
+        this.selectAllVariables = true;
+      }
+      else {
+        $('#selectAll').prop('indeterminate', false);
+        this.selectAllVariables = false;
+      }
+    },
     download: function () {
       // TODO
     }
--- start-dotnet.sh
+++ start-dotnet.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 export ASPNETCORE_ENVIRONMENT=Production 
-cd /var/www
 dotnet Usa.chili.Web.dll
